825f5fdb34563a5007281b4c2d5fb4f45c3e5861
Yuliia Tarasiuk <juls@Yuliias-MacBook-Pro.local>
2019-03-15 04:40:36 +0200

    Updates

---
+++ precache-manifest.6499a51ce00058a7cab41855ce629086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d125f4ee65b9fc53993",
+    "url": "/JulsT/star-wars/static/js/runtime~main.791c51f2.js"
+  },
+  {
+    "revision": "a6187aa88079eaedee4f",
+    "url": "/JulsT/star-wars/static/js/main.7d231e2c.chunk.js"
+  },
+  {
+    "revision": "ed77ce981a0867c67545",
+    "url": "/JulsT/star-wars/static/js/2.5b7b31f2.chunk.js"
+  },
+  {
+    "revision": "a6187aa88079eaedee4f",
+    "url": "/JulsT/star-wars/static/css/main.cab9226d.chunk.css"
+  },
+  {
+    "revision": "ed77ce981a0867c67545",
+    "url": "/JulsT/star-wars/static/css/2.03756c1a.chunk.css"
+  },
+  {
+    "revision": "07558f35d80b418cdf6ae5a95552539e",
+    "url": "/JulsT/star-wars/index.html"
+  }
+];
